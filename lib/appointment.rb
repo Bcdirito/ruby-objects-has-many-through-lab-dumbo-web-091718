d6398a4ea0839356fdc14211fc8b2a2b8d8185eb
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 16:51:16 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,3 +1,15 @@
 class Appointment
+  attr_reader :doctor, :patient, :date
   
+  @@all = []
+  
+  def initialize(doctor, patient, date)
+    @doctor = doctor
+    @patient = patient
+    @date = date
+  end
+  
+  def self.all
+    @@all
+  end
 end
